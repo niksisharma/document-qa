122315618dd29ab36818bac072e44fc85b9ca0e3
Nikita Sharma <51536421+niksisharma@users.noreply.github.com>
2025-09-11 14:41:42 +0000

    lab 3a

---
+++ lab3.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from openai import OpenAI
+
+# Show title and description.
+st.title("📄 Nikita's ChatBot Lab 3")
+
+openai_api_key = st.secrets["DB_TOKEN"]
+
+openAI_model = st.sidebar.selectbox("Which Model?",("mini","regular"))
+model = "gpt-4o" if openAI_model == "regular" else "gpt-4o-mini"
+
+    
+if 'client' not in st.session_state:
+    st.session_state.client = OpenAI(api_key=openai_api_key)
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    chat_msg = st.chat_message(msg["role"])
+    chat_msg.write(msg["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": "prompt"})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    client = st.session_state.client
+    stream = client.chat.completions.create(
+        model = model,
+        messages = st.session_state.messages,
+        stream = True
+    )
+
+    response = "response"
+    
+    with st.chat_message("assistant"):
+        response = st.write_stream(stream)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 lab1_page = st.Page("lab1.py", title="Lab 1", icon="🖥️")
-lab2_page = st.Page("lab2.py", title="Lab 2", icon="🖥️", default=True)
+lab2_page = st.Page("lab2.py", title="Lab 2", icon="🖥️")
+lab3_page = st.Page("lab3.py", title="Lab 3", icon="🖥️", default=True)
 
-pg = st.navigation([lab1_page, lab2_page])
+pg = st.navigation([lab1_page, lab2_page, lab3_page])
 st.set_page_config(page_title="Nikita's Labs", page_icon="📄")
 pg.run()
